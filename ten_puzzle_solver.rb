ecd9e1633c9868a1ff3ed0ed7d1699ca806221f2
eityans <eityans@gmail.com>
2022-11-28 11:13:34 +0900

    decode_poland

--- ten_puzzle_solver.rb
+++ ten_puzzle_solver.rb
@@ -8,6 +8,7 @@ def is_operator?(char)
 end
 
 # calc_poland("12+3+4*")
+# => 24
 def calc_poland(poland_str)
   stack = []
   poland_str.split('').each do |c|
@@ -35,4 +36,29 @@ def calc_poland(poland_str)
   stack.first
 end
 
+# decode_poland("12+3+4*")
+# => (1+2+3)*4
+def decode_poland(poland_str)
+  space = []
+  poland_str.split('').each do |c|
+    if is_operator?(c)
+      second = space.pop
+      first = space.pop
+
+      if ["*", "/"].include?(c)
+        first = first.size > 1 ? "("+first+")" : first
+        second = second.size > 1 ? "("+second+")" : second
+      end
+
+      result = first + c + second
+      space << result
+    else
+      space << c
+    end
+  end
+
+  space.join
+end
+
 pp calc_poland("12+3+4*")
+pp decode_poland("12+3+4*")
